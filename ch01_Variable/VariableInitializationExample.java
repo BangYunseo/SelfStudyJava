697cba1d7535faa3a625213f5306c45f57d4f88f
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-03-11 22:21:21 +0900

    Create VariableInitializationExample.java

---
+++ ch01_Variable/VariableInitializationExample.java
@@ -0,0 +1,15 @@
+package sec01.exam01;
+
+public class VariableInitializationExample {
+	public static void main(String[] args) {
+		// int형 변수 value 선언 후 20으로 초기화 
+		int value = 20;    
+
+    // int형 변수 result 선언 후 변수 value와 10 덧셈 진행
+		int result = value + 10;
+		System.out.println(result);
+	}
+}
+
+// 출력 예시
+// 30
