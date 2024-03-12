b5f1610239a6e90610d86d4e0e2f6809fe38a4ee
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-03-12 22:07:34 +0900

    Create StringExample.java

---
+++ ch02_PrimitiveType/StringExample.java
@@ -0,0 +1,14 @@
+package sec02.exam05;
+
+public class StringExample {
+	public static void main(String[] args) {
+		String name = "김자바";
+		String job = "프로그래머";
+		System.out.println(name);
+		System.out.println(job);
+	} 
+}
+
+// 출력 예시
+// 김자바
+// 프로그래머
