f12cbe71257c5d1465fcd2c34fef8ea66f4580e8
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-03-19 22:45:02 +0900

    Update CastingExample.java

--- ch03_TypeCasting/CastingExample.java
+++ ch03_TypeCasting/CastingExample.java
@@ -1,18 +1,23 @@
 package sec03.exam02;
 
 public class CastingExample {
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+		// 강제 타입 변환
+		
 		int intValue = 44032;
 		char charValue = (char) intValue;
 		System.out.println(charValue);
+		// int형으로 44032인 값을 char형으로 : 가
 		
 		long longValue = 500;
 		intValue = (int) longValue;
 		System.out.println(intValue);
+		// long형으로 500인 값을 int형으로 : 500
 		
 		double doubleValue = 3.14;
 		intValue = (int) doubleValue;
 		System.out.println(intValue);	
+		// double형으로 3.14인 값을 int형으로 : 3(뒤에 소수점이 짤림)
 	} 
 }
 
