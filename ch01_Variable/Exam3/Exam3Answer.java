a6c7340473afb815ce62973be7c3d34ea4cf5e01
BangYunseo <yunseobang33@gmail.com>
2024-08-26 17:31:45 +0900

    mod

---
+++ ch01_Variable/Exam3/Exam3Answer.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("첫번째 수 : ");
+      String strNum1 = scanner.nextLine();
+      
+      System.out.print("두번째 수 : ");
+      String strNum2 = scanner.nextLine();
+      
+      int num1 = Integer.parseInt(strNum1);
+      int num2 = Integer.parseInt(strNum2);
+      int result = num1 + num2;
+      System.out.println("덧셈 결과 : " + result);
+    }
+}
